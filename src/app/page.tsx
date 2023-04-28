d7546034de2e027f324d6998d97ff93764b11535
tawandachiteshe <dickenstawanda@gmail.com>
2023-04-28 22:55:56 +0200

    added responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,112 +5,152 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className="w-full">
+          <div className="grid md:mx-36 mx-5 mt-5 grid-cols-1">
+
+              <div className="grid grid-cols-3" >
+                  <div className="col-span-2" >
+                      <h1 className="text-3xl font-bold">
+                          SportsCraft
+                      </h1>
+                  </div>
+
+                  <div className="grid max-[1200px]:hidden gap-1 grid-cols-4">
+                      <a href='#' className="hover:text-blue-200 hover:underline hover:underline-offset-4 hover:decoration-amber-100">
+                          Services
+                      </a>
+                      <a href='#' className="hover:text-blue-200 hover:underline hover:underline-offset-4 hover:decoration-amber-100">
+                          Clients
+                      </a>
+                      <a href='#' className="hover:text-blue-200 hover:underline hover:underline-offset-4 hover:decoration-amber-100">
+                          About
+                      </a>
+                  </div>
+                  <div className="md:hidden grid justify-end justify-items-end">
+                      <button type="button"
+                              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                          <svg aria-hidden="true" className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 18L20 18" stroke="#FFF" stroke-width="2" stroke-linecap="round"></path> <path d="M4 12L20 12" stroke="#FFF" stroke-width="2" stroke-linecap="round"></path> <path d="M4 6L20 6" stroke="#FFF" stroke-width="2" stroke-linecap="round"></path> </g></svg>
+                          <span className="sr-only">Icon description</span>
+                      </button>
+                  </div>
+              </div>
+
+              <div className="grid  md:grid-cols-2 gap-2 mt-32">
+
+                  <div className="grid justify-items-center content-center">
+                      <div className="relative  h-48 w-48 md:h-96 md:w-96">
+                          <Image
+                              src="/Businessdevelopment.jpg"
+                              alt="Logo"
+                              layout="fill" // required
+                              objectFit="cover" // change as you like
+                              // you can use other classes here too
+                          />
+                      </div>
+                  </div>
+
+                  <div className="md:order-first">
+                      <h1 className="text-5xl text-blue-200 font-extrabold">
+                          BUSINESS DEVELOPMENT
+                      </h1>
+                      <p className="mt-5">
+                          Our team is committed to helping our athlete and corporate clients successfully navigate
+                          the many facets of the sports marketing, events and sponsorship industries.
+                      </p>
+                      <div className="mt-5">
+                          <button className="font-bold py-2 px-4 rounded bg-blue-200 text-black">
+                              Read more
+                          </button>
+                          <button className="font-bold ml-2 py-2 px-4 rounded bg-blue-200 text-black">
+                              Contact us {">"}
+                          </button>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="grid md:grid-cols-2 mt-24">
+                  <div className="grid justify-center content-center">
+                      <div className="relative  h-48 w-48 md:h-96 md:w-96">
+                          <Image
+                              src="/Sportsbusinessconsultancy.jpg"
+                              alt="Logo"
+                              layout="fill" // required
+                              objectFit="cover" // change as you like
+                              // you can use other classes here too
+                          />
+                      </div>
+                  </div>
+                  <div>
+                      <h1 className="text-xl mt-5 uppercase font-bold">
+                          Sports business consultancy
+                      </h1>
+                      <p className="mt-5">
+                          SportsCraft Business Development consults on behalf of corporations in strategically identifying sports and entertainment marketing, sponsorship, licensing and athlete/entertainer partnership opportunities. We also represent our corporate clients in the negotiation and execution of the contracts and to fulfill sponsorships. We have unique connections and exceptional influence to help build a company’s brand and increase revenues by reaching the desired audience through effective sports and entertainment marketing.
+                      </p>
+                  </div>
+              </div>
+
+              <div className="grid md:grid-cols-2 gap-5 mt-24">
+
+                  <div>
+                      <div className="grid justify-items-center content-center">
+                          <div className="relative  h-48 w-48 md:h-96 md:w-96">
+                              <Image
+                                  src="/Sponsorshipdealflow.jpg"
+                                  alt="Logo"
+                                  layout="fill" // required
+                                  objectFit="cover" // change as you like
+                                  // you can use other classes here too
+                              />
+                          </div>
+                      </div>
+                  </div>
+
+                  <div className="md:order-first mt-5">
+                      <h1 className="text-xl uppercase font-bold">
+                          Sponsorship deal flow
+                      </h1>
+                      <p className="mt-5">
+                          SportsCraft Business Development provides assistance in sponsorship procurement for various sports and entertainment entities including, but not limited to, events, television programming, properties, conventions, athletes and entertainers and teams.  We understand the complexities of sponsorship sales and have developed a strategic method using our extensive contact list to effectively sell sponsorship for multi-faceted sports and entertainment properties.
+                      </p>
+                  </div>
+              </div>
+
+              <div className="grid md:grid-cols-2 gap-5 mt-24">
+                  <div className="grid justify-center  content-center">
+                      <div className="relative  h-48 w-48 md:h-96 md:w-96">
+                          <Image
+                              src="/event_management.jpg"
+                              alt="Logo"
+                              layout="fill" // required
+                              objectFit="cover" // change as you like
+                              // you can use other classes here too
+                          />
+                      </div>
+                  </div>
+                  <div>
+                      <h1 className="text-xl uppercase font-bold">
+                          Event management
+                      </h1>
+                      <p className="mt-5">
+                          SportsCraft Business Development provides assistance in sponsorship procurement for various sports and entertainment entities including, but not limited to, events, television programming, properties, conventions, athletes and entertainers and teams.  We understand the complexities of sponsorship sales and have developed a strategic method using our extensive contact list to effectively sell sponsorship for multi-faceted sports and entertainment properties.
+                      </p>
+                  </div>
+              </div>
+
+              <div className="grid mt-5">
+                  <div className="h-96">
+                  </div>
+
+              </div>
+          </div>
+          <div className="grid h-24 text-black bg-blue-200 justify-items-center content-center">
+              <p className="text-xs ">©SportsCraft, 2023</p>
+          </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }
